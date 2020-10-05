55f80caa228227c244df318e986ee6c3b35a3e4c
KostasKA2020 <konstakozh@yandex.ru>
2020-10-05 17:49:24 +0400

    fix project

--- KozhevnikovK/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import chatReducer from './chatReducer';
-
-const rootReducer = combineReducers({
-  chats: chatReducer,
-});
-
-export default rootReducer;
